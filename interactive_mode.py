1fb7f6766ec5eda9c8825ad42b4bc3d3d5b71271
T1 <126980610+tadash10@users.noreply.github.com>
2023-07-23 12:34:56 -0300

    Create interactive_mode.py

---
+++ interactive_mode.py
@@ -0,0 +1,9 @@
+def review_changes(changes_needed):
+    # Allow users to interactively review and confirm individual changes before applying them
+
+def modify_desired_state():
+    # Implement an interactive mode that allows users to modify the desired state during runtime
+
+def confirm_apply_changes():
+    # Ask the user for confirmation before applying changes
+    # Return True if the user confirms, False otherwise
