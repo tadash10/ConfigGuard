eb230a7395a2d44ae3edc2e25530e28e77ce5c42
T1 <126980610+tadash10@users.noreply.github.com>
2023-07-23 12:37:07 -0300

    Create automated_testing.py

---
+++ automated_testing.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest.mock import patch
+import desired_state
+
+class TestDesiredState(unittest.TestCase):
+    # Implement unit tests for desired_state functions
+
+class TestInteractiveMode(unittest.TestCase):
+    # Implement unit tests for interactive_mode functions
+
+if __name__ == '__main__':
+    unittest.main()
